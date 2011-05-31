42d9225f2aed557c84134db40504e1af7e9b515b
Nikolay Kim <fafhrd91@gmail.com>
2011-05-31 16:15:19 -0500

    moved to seperate repositories

--- memphis.config/memphis/config/zca.py
+++
@@ -1,8 +0,0 @@
-""" zca configuration """
-from zope.component.event import objectEventNotify
-
-from memphis.config import api
-from memphis.config.directives import action
-
-
-action(api.registerHandler, objectEventNotify)
